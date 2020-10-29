64826f4ee8ca98ba7211d5700e63a7e776f93b64
wangsuhang <513605732@qq.com>
2020-10-29 20:57:59 +0800

    A over in dev

---
+++ src/main/java/HelloGit4.java
@@ -0,0 +1,10 @@
+/**
+ * @author suahng
+ * @date 2020-10-29 20:57
+ * @dec
+ */
+public class HelloGit4 {
+    public static void main(String[] args){
+        System.out.println("sdkhfv");
+    }
+}
