2aa96b2091b07f8fad17f0f13d51fdcea2402fde
wangsuhang <513605732@qq.com>
2020-10-29 21:03:21 +0800

    A over in dev

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("skhf");
+        System.out.println("fdsgh");
     }
 }
