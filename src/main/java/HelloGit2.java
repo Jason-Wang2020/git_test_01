6fdee196cfec80054da42907c928643f2e526beb
wangsuhang <513605732@qq.com>
2020-10-29 20:45:18 +0800

    A over in dev

--- src/main/java/HelloGit2.java
+++ src/main/java/HelloGit2.java
@@ -6,4 +6,7 @@ import java.sql.SQLOutput;
  * @dec
  */
 public class HelloGit2 {
+    public static void main(String[] args){
+        System.out.println("wang su hang");
+    }
 }
---
+++ src/main/java/HelloGit3.java
@@ -0,0 +1,10 @@
+/**
+ * @author suahng
+ * @date 2020-10-29 20:42
+ * @dec
+ */
+public class HelloGit3 {
+    public static void main(String[] args){
+        System.out.println("sfklhs");
+    }
+}
