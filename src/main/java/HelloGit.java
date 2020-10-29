8cbcfd0b2277c41b44368ee490ae841a6551119e
wangsuhang <513605732@qq.com>
2020-10-29 20:34:02 +0800

    first commit

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @author suahng
+ * @date 2020-10-29 20:16
+ * @dec
+ */
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
